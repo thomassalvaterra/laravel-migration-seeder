8f4a07d4470cbecba581c728c3d27939e1492e87
Thomas66 <thomassalvaterra2001@gmail.com>
2024-01-24 10:44:48 +0100

    insert dates in page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,38 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<h1>Dati da config/data.js: {{ $home }}</h1>
-<p>This is my paragraph content.</p>
-<button class="btn btn-primary">Cliccami</button>
-@endsection
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap-grid.min.css">
+    <title>Train</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            @foreach ($train as $trains)
+            <div class="col-2 ">
+                <h1>{{$trains->agency}}</h1>
+                <p>{{$trains->departure_station}}</p>
+                <p>{{$trains->arrival_station}}</p>
+                <p>{{$trains->departure_time}}</p>
+                <p>{{$trains->arrival_time}}</p>
+                <p>{{$trains->train_code}}</p>
+                <p>{{$trains->carriage_number}}</p>
+                <p>{{$trains->train_cancelled}}</p>
+                <p>{{$trains->in_time}}</p>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</body>
+<style scoped>
+    body {
+        background-color: #222;
+        color: white;
+    }
+</style>
+
+</html>
